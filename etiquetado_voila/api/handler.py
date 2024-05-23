5c606de37bd8b8262366b8e6e866d5eb46f6174f
AlbertEngstfeld <albert.engstfeld@gmail.com>
2024-05-23 11:46:10 +0200

    Add file deletion handler

--- etiquetado_voila/api/handler.py
+++ etiquetado_voila/api/handler.py
@@ -1,17 +1,21 @@
-from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from pathlib import Path
 
 
 class FileCreationHandler(FileSystemEventHandler):
 
     def __init__(self, app):
-        r"""``*processing_callbacks`` are python methods which interact and process
-        newly created files with the specified ``suffix`` detected by watchdog."""
         self.app = app
 
     def on_created(self, event):
         self.app.file_create(event.src_path)
+
+    def on_deleted(self, event):
+        self.app.file_deleted(event.src_path)
+
+class FileDeletionHandler(FileSystemEventHandler):
+
+    def __init__(self, app):
+        self.app = app
 
     def on_deleted(self, event):
         self.app.file_deleted(event.src_path)
